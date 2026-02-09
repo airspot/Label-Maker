d8838984401a921482ce0432b294fe4f6f702c72
airspot <50253761+airspot@users.noreply.github.com>
2026-02-09 11:12:26 +0100

    label_app.py

--- label_app.py
+++ label_app.py
@@ -9,7 +9,7 @@ from typing import List, Tuple
 # ==========================================
 class Design:
     # Branding Colors
-    RED = "#42a5f5"      
+    RED = "#ef5350"      
     BLUE = "#42a5f5"     
     SOFT_GRAY = "#F1F5F9" 
     DARK_TEXT = "#000000"
